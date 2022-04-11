90ece52d27f2ad3d13e77c32ec8e5b687d3204c3
Christina Bukas <ga27loj@mytum.de>
2022-04-11 09:28:31 +0200

    removed skimaege from install requirements, already in napari[all]

--- napari_organoid_counter/_tests/test_widget.py
+++ napari_organoid_counter/_tests/test_widget.py
@@ -8,8 +8,8 @@ def test_organoid_counter_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
     viewer.add_image(np.random.random((100, 100)))
 
-    # # create our widget, passing in the viewer
-    # my_widget = OrganoidCounterWidget(viewer)
+    # create our widget, passing in the viewer
+    my_widget = OrganoidCounterWidget(viewer)
 
     # # call our widget method
     # my_widget._preprocess()
@@ -23,8 +23,8 @@ def test_organoid_counter_widget(make_napari_viewer, capsys):
     # '''TO DO'''
     # my_widget._on_run_click()
     # '''TO DO'''
-    # my_widget._on_downsampling_changed()
-    # assert my_widget.downsampling == my_widget.downsampling_slider.value()
+    my_widget._on_downsampling_changed()
+    assert my_widget.downsampling == my_widget.downsampling_slider.value()
 
     # my_widget._on_diameter_changed()
     # assert my_widget.min_diameter == my_widget.min_diameter_slider.value()
