1d7b0538ed6293f5c712345310d07ae0ccc830e6
Christina Bukas <ga27loj@mytum.de>
2023-07-14 15:47:47 +0200

    added new model

--- napari_organoid_counter/settings.py
+++ napari_organoid_counter/settings.py
@@ -4,7 +4,8 @@ def init():
     
     global MODELS
     MODELS = {
-        "default": {"filename": "model_v1.ckpt", "source": "https://zenodo.org/record/7708763/files/model_v1.ckpt"}
+        "model_1 (default)": {"filename": "model_v1.ckpt", "source": "https://zenodo.org/record/7708763/files/model_v1.ckpt"},
+        "model_2": {"filename": "model_v2.ckpt", "source": "https://zenodo.org/record/8146857/files/model_v2.ckpt"},
     }
     
     global MODELS_DIR
